8e6dae44a393a1e4348008d5f04851c48fc47ff4
Jan Brezina <jan.brezina@tul.cz>
2025-08-18 18:15:59 +0200

    Fix other file moves.

--- app/hlavo_surface/inputs/__init__.py
+++ app/hlavo_surface/inputs/__init__.py
@@ -8,6 +8,4 @@ work_dir.mkdir(parents=True, exist_ok=True)
 # Following is public
 input_dir = __script_dir__.parent / "inputs"
 surface_schema_yaml = input_dir / "surface_schema.yaml"
-surface_schema_bukov_yaml = input_dir / "surface_schema_bukov.yaml"
 odyssey_locations_csv = input_dir / "odyssey_locations.csv"
-bukov_locations_csv = input_dir / "bukov_locations.csv"
