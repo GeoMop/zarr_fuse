a7c567d0a3e6cad3b54182fda62589326137e459
mfatihakbas <119815601+mfatihakbas@users.noreply.github.com>
2025-10-18 17:32:14 +0300

    save

--- zarr_fuse/logger.py
+++ zarr_fuse/logger.py
@@ -93,8 +93,7 @@ class StoreLogHandler(logging.Handler):
 
 
         # choose initial append strategy
-        if isinstance(store, (zarr.storage.LocalStore,)):
-            assert store.supports_partial_writes, "Store does not support partial writes"
+        if isinstance(store, (zarr.storage.LocalStore,)) and getattr(store, 'supports_partial_writes', False):
             self._append = self._append_unsafe
         else:
             self._append = self._append_safe
