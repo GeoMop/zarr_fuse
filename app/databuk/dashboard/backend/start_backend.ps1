0dfa1bd110e66eee204d5a2220bd2bf13989f1d8
mfatihakbas <119815601+mfatihakbas@users.noreply.github.com>
2025-11-22 17:26:21 +0300

    endpoint url added to credantials script

--- app/databuk/dashboard/backend/start_backend.ps1
+++ app/databuk/dashboard/backend/start_backend.ps1
@@ -3,5 +3,7 @@ $env:S3_BUCKET_NAME = "your_bucket_name"
 $env:ZF_S3_ACCESS_KEY = "your_access_key"
 $env:ZF_S3_SECRET_KEY = "your_secret_key"
 
+$env:ZF_S3_ENDPOINT_URL = "https://s3.your-enpoint.url"
+
 # Run backend
 python run.py
