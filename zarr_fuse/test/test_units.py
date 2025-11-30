Fixed units registry

zarr_fuse/test/test_units.py
@@ -1,8 +1,18 @@
 import numpy as np
-import pint
 import pytest
 
 from zarr_fuse import units
+
+def test_quanity():
+    q = units.Quantity(np.array([1, 2, 3]), 'meter')
+    assert isinstance(q, units.Quantity)
+    assert np.allclose(q.to('centimeter').magnitude, [100, 200, 300])
+
+    # Interoperability with pint Quantity, common registry sould be used due to use of set_application_registry
+    raw_q = units._pint.Quantity(np.array([1, 2, 3]), 'meter')
+    assert np.all(raw_q == q)
+
+
 
 # TODO: replace by Variable.convert_value
 @pytest.mark.skip
@@ -10,7 +20,7 @@ def test_create_quantity():
     from datetime import timezone, timedelta
     # Numeric conversion correctness
     q_numeric = units.create_quantity(['1', '2.5', '3'], 'meter')
-    assert isinstance(q_numeric, pint.Quantity)
+    assert isinstance(q_numeric, units.Quantity)
     assert np.allclose(q_numeric.to('millimeter').magnitude, [1000, 2500, 3000])
 
 
@@ -28,6 +38,11 @@ def test_create_quantity():
     check_dt(
         cfg=units.DateTimeUnit(tick, tz='+00:00', dayfirst=False, yearfirst=False),
         dates=['2021-12-31 23:00', '31 Dec 2021 23:00'],
+        expected_val=expected
+    )
+    check_dt(
+        cfg=units.DateTimeUnit(tick, tz='+00:00', dayfirst=False, yearfirst=False),
+        dates=['2021-12-31T23:00:00', '31 Dec 2021 23:00'],
         expected_val=expected
     )
     check_dt(
@@ -65,7 +80,7 @@ def test_create_quantity():
     assert np.array_equal(qd_m.magnitude, expected_m)
 
     # Test __add__
-    addition = units.ureg.Quantity(90, 'minute')
+    addition = units.Quantity(90, 'minute')
     qd_added = qd_m + addition
     expected_added = np.array(['2022-01-01T00:30', '2022-01-01T00:30'], dtype='datetime64[m]')
     assert np.array_equal(qd_added.magnitude, expected_added)

zarr_fuse/units.py
@@ -3,19 +3,20 @@ from functools import cached_property
 from typing import Iterable, Any
 
 import numpy as np
-import pint
-from pint import UndefinedUnitError
 import datetime
 import dateutil
 import attrs
 import time
 
-ureg = pint.UnitRegistry()
+import pint as _pint
+ureg = _pint.UnitRegistry()
 
 ureg.define('bool = []')
 ureg.define('boolean = bool')          # alias
 ureg.define('true  = 1 bool')         # allow parsing "true"
 ureg.define('false = 0 bool')
+
+_pint.set_application_registry(ureg)
 
 
 # Map common timezone abbreviations to fixed-offset tzinfo
@@ -58,15 +59,42 @@ def build_tzinfos():
 
 TZINFOS = build_tzinfos()
 
-class Unit(pint.Unit):
+class Unit(ureg.Unit):
     def asdict(self, value_serializer, filter):
         return str(self)
+
+    def default_dtype(self):
+        return 'float64'
 
     def delta_unit(self):
         return self
 
     def delta_dtype(self, dtype):
         return dtype
+
+class NoneUnit:
+    def __init__(self):
+        pass
+
+    def asdict(self, value_serializer, filter):
+        return 'None'
+
+    def default_dtype(self):
+        return None
+
+    def delta_unit(self):
+        return None
+
+    def delta_dtype(self, dtype):
+        return None
+
+
+class Quantity(ureg.Quantity):
+
+    @property
+    def unit(self):
+        # pint.Quantity.units has unlogical name.
+        return self.units
 
 @attrs.define
 class DateTimeUnit:
@@ -87,7 +115,7 @@ class DateTimeUnit:
         """
         val = self.tz
         if val is None:
-            return None
+            return datetime.timezone.utc
 
         # offset form ±HH:MM
         m = re.match(r'([+-])(\d{2}):(\d{2})$', val)
@@ -112,11 +140,37 @@ class DateTimeUnit:
         offset = tzinfo.utcoffset(datetime.datetime.now())
         return offset.total_seconds() / 3600.0
 
+    def default_dtype(self):
+        return f'datetime64[{self.tick}]'
+
     def delta_unit(self):
         return Unit(self.tick)  # TODO: change from 'str' to pint.Unit in DataTimeUnit
 
     def delta_dtype(self, dtype):
         return np.dtype(f'timedelta64[{self.tick}]')
+
+    def nat(self):
+        """Return the NaT value for this DateTimeUnit."""
+        return np.datetime64('NaT', self.tick)
+
+    def parse(self, value):
+        v = str(value)
+        if str(v) == 'NaT':  #is in ['NaT', 'nat', 'null', 'None']:
+            # NaT values are valud
+            return self.nat()
+
+        dt = dateutil.parser.parse(v,
+                                   dayfirst=self.dayfirst,
+                                   yearfirst=self.yearfirst,
+                                   tzinfos=TZINFOS)
+        # If no explicit tz, assign dt_unit tz (or UTC if none)
+        if dt.tzinfo is None:
+            target_tz = self.tzinfo
+            dt = dt.replace(tzinfo=target_tz)
+        # Convert any explicit tz to dt_unit.tz (or UTC)
+        dt_utc = dt.astimezone(self.tzinfo)
+        # Drop tzinfo to store as numpy.datetime64 local times
+        return dt_utc.replace(tzinfo=None)
 
 class DateTimeQuantity:
     """
@@ -168,9 +222,10 @@ class DateTimeQuantity:
         """
         Add a timedelta expressed as a pint.Quantity to this DateTimeQuantity.
         """
-        if not isinstance(other, pint.Quantity):
+        if not isinstance(other, [Quantity, ureg.Quantity]):
             return NotImplemented
         # Ensure dimension is time
+        # TODO: simplify tis conversion
         other_sec = other.to('second').magnitude
         # Use time module to handle leap seconds correctly
         # Convert seconds to microseconds
@@ -186,27 +241,22 @@ class DateTimeQuantity:
                 f"unit={self._unit}h>")
 
 
-def _create_dt_quantity(values, dt_unit):
+
+def _create_dt_quantity(values, dt_unit: DateTimeUnit, log:'SchemaCtx') -> DateTimeQuantity:
     """
     Create a DateTimeQuantity from a numpy array of datetime64 values and a DateTimeUnit.
     """
     parsed = []
+    last_valid = None
     for val in values:
-        dt = dateutil.parser.parse(str(val),
-                          dayfirst=dt_unit.dayfirst,
-                          yearfirst=dt_unit.yearfirst,
-                          tzinfos=TZINFOS)
-        # If no explicit tz, assign dt_unit tz (or UTC if none)
-        if dt.tzinfo is None:
-            target_tz = dt_unit.tzinfo or datetime.timezone.utc
-            dt = dt.replace(tzinfo=target_tz)
-        # Convert any explicit tz to dt_unit.tz (or UTC)
-        final_tz = dt_unit.tzinfo or datetime.timezone.utc
-        dt_utc = dt.astimezone(final_tz)
-        # Drop tzinfo to store as numpy.datetime64 local times
-        dt_naive = dt_utc.replace(tzinfo=None)
-        parsed.append(dt_naive)
-    np_dates = np.array([np.datetime64(dt, dt_unit.tick) for dt in parsed])
+        try:
+            dt_value = dt_unit.parse(val)
+            last_valid = dt_value
+        except dateutil.parser.ParserError as e:
+            log.error(f"Failed to parse datetime value: {str(val)}; last valid date time: {last_valid}")
+            dt_value = dt_unit.nat()
+        parsed.append(dt_value)
+    np_dates = np.array(parsed, dtype=f'datetime64[{dt_unit.tick}]')
     return DateTimeQuantity(np_dates, dt_unit)
 
 

zarr_fuse/zarr_schema.py
@@ -52,7 +52,8 @@ def unit_instance(cfg: ContextCfg, default_unit=None) -> Unit:
         return default_unit
     elif isinstance(unit, str):
         try:
-            return units.Unit(unit)
+            u = units.Unit(unit)
+            return u
         except Exception as e:
             cfg.schema_ctx.error(f"Invalid unit string: {unit}. Pint error: {e}")
     elif isinstance(unit, dict):
@@ -195,6 +196,9 @@ class Interval(AddressMixin):
 
 @attrs.define
 class IntervalRange(Interval):
+    """
+    TODO: support and test DateTime ranges.
+    """
     def asdict(self, value_serializer, filter):
         return {'interval': Interval.asdict(self, value_serializer, filter)}
 
@@ -212,7 +216,7 @@ class IntervalRange(Interval):
         return codes
 
 
-Quantity = units.pint.Quantity | units.DateTimeQuantity
+Quantity = units.Quantity | units.DateTimeQuantity
 
 
 @attrs.define
@@ -311,21 +315,21 @@ class Variable(AddressMixin):
 
         if isinstance(from_unit, units.DateTimeUnit):
             # DateTime specialization
-            quantity = units._create_dt_quantity(values, from_unit)
+            quantity = units._create_dt_quantity(values, from_unit, log=self._address)
         else:
             if dtype is not None:
                 values = to_typed_array(values, dtype, self._address)
 
             # Pint specialization when a unit string is provided
-            assert isinstance(from_unit, units.pint.Unit)
-            quantity = units.ureg.Quantity(values, from_unit)
+            assert isinstance(from_unit, units.Unit)
+            quantity = units.Quantity(values, from_unit)
         return quantity
 
     def magnitude(self, q: np.ndarray | Quantity) -> np.ndarray:
         """
         Return the magnitude of the provided quantity, converting to the variable's unit if needed.
         """
-        if isinstance(q, (units.pint.Quantity, units.DateTimeQuantity)):
+        if isinstance(q, (units.Quantity, units.DateTimeQuantity)):
             return q.magnitude
         else:
             return q
@@ -335,7 +339,7 @@ class Variable(AddressMixin):
         """
         Return the provided quantity, converting to the variable's unit if needed.
         """
-        if isinstance(q, (units.pint.Quantity, units.DateTimeQuantity)):
+        if isinstance(q, (units.Quantity, units.DateTimeQuantity)):
             return q.to(self.unit)
         else:
             return self._make_quantity(q, from_unit=self.unit)