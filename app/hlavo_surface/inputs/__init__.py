bf283144e045c954f0a84d1241eaa6575cd90ea3
Jan Brezina <jan.brezina@tul.cz>
2025-12-03 01:30:13 +0100

    Update schemas

--- app/hlavo_surface/inputs/__init__.py
+++ app/hlavo_surface/inputs/__init__.py
@@ -7,5 +7,5 @@ work_dir.mkdir(parents=True, exist_ok=True)
 
 # Following is public
 input_dir = __script_dir__.parent / "inputs"
-surface_schema_yaml = input_dir / "surface_schema.yaml"
+surface_schema_yaml = input_dir / "surface_schema_v3.yaml"
 odyssey_locations_csv = input_dir / "odyssey_locations.csv"
